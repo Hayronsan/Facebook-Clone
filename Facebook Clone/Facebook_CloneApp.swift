2bf16b11a6cc4070a9154ec710c7ea58381f3c9a
Hayronsan <hayronsan@gmail.com>
2024-04-25 17:23:27 +0500

    Initial Commit

---
+++ Facebook Clone/Facebook_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facebook_CloneApp.swift
+//  Facebook Clone
+//
+//  Created by Nodir on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Facebook_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
