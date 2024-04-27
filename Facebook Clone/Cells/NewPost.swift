5c1d00fa71db22977841e9d198745721d6ea0cf7
Hayronsan <hayronsan@gmail.com>
2024-04-28 02:24:19 +0500

    Facebook clone

---
+++ Facebook Clone/Cells/NewPost.swift
@@ -0,0 +1,18 @@
+//
+//  NewPost.swift
+//  Facebook Clone
+//
+//  Created by Nodir on 27/04/24.
+//
+
+import SwiftUI
+
+struct NewPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewPost()
+}
